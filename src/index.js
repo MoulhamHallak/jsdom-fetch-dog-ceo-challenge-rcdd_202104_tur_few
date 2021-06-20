51cd5ca95f21ee5d56e9a10f814c55b87a1319c6
Moulham Hallak <mou990@gmail.com>
2021-06-20 23:18:57 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -9,11 +9,64 @@ function fetchPhotos() {
 console.log(json)
 });
 
-function dogImages(dogs){
-    dogs.forEach (dog => {
-      const dogsContainer = document.querySelector('#dog-image-container')
-      const dogs = document.createElement('img')
-      dogs.src = dog
-      dogsContainer.appendChild(dogs)
+function renderImg(images) {
+    const imgDiv = document.getElementById("dog-image-container");
+    images.forEach(image => {
+        const imgTag = document.createElement("img")
+        imgTag.src = image
+        imgDiv.appendChild(imgTag)
     });
-  }
+}
+
+function fetchBreeds() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then(response => response.json())
+    .then(json => renderBreeds(json));
+}
+
+function renderBreeds(breeds) {
+    const breedsUl = document.getElementById("dog-breeds");
+    breedKeys = Object.keys(breeds.message)
+    breedKeys.forEach(breed => {
+        breedsArr.push(breed);
+        const liTag = document.createElement("li")
+        liTag.innerText = breed
+        breedsUl.appendChild(liTag)
+    });
+}
+
+function filterBreeds(letter) {
+    const breedsUl = document.getElementById("dog-breeds");
+    let child = breedsUl.lastElementChild;
+    while (child) {
+        breedsUl.removeChild(child);
+        child = breedsUl.lastElementChild;
+    };
+    let filtered = breedsArr.filter(breed => breed.startsWith(letter));
+    filtered.forEach(breed => {
+        const liTag = document.createElement("li")
+        liTag.innerText = breed
+        breedsUl.appendChild(liTag)
+    });
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const breedList = document.getElementById("dog-breeds");
+    const ulChildren = breedList.children;
+    const select = document.getElementById("breed-dropdown")
+
+    select.addEventListener("change", function(event) {
+        filterBreeds(event.target.value);
+    });
+
+    breedList.addEventListener("click", function(event){
+        event.target.style.color = "green";
+        });
+
+    fetchImages();
+    fetchBreeds();
+  });
