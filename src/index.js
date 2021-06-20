4f99a7aa01e0fe87362eeeb8cabe76592dfb02cb
Moulham Hallak <mou990@gmail.com>
2021-06-20 23:18:36 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", () => {
+function fetchPhotos() {
+  return fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(resp => resp.json())
+  .then(json => renderPhotos(json));
+}
+console.log(json)
+});
+
+function dogImages(dogs){
+    dogs.forEach (dog => {
+      const dogsContainer = document.querySelector('#dog-image-container')
+      const dogs = document.createElement('img')
+      dogs.src = dog
+      dogsContainer.appendChild(dogs)
+    });
+  }
